d4691b95856287ee20ea6a2ba36485da14cef635
Ehtesham Siddiqui <esiddiqui@touchbistro.com>
2024-04-07 21:05:52 -0400

    remove set.rs & add doctests

--- src/collections.rs
+++ src/collections.rs
@@ -1,10 +1,8 @@
 
 // inner modules here
 pub mod singly_linked_list; 
-pub mod set; 
 
 
 
 // type re-exports here for external organization
 pub use singly_linked_list::LinkedList;
-pub use set::Set;
